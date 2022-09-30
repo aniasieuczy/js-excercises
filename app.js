7e8c5ab91c2ffa75580ca8c2c43700659e4007b1
Anna Szpecht <aszpecht@sii.pl>
2022-09-30 13:22:49 +0200

    getCount added

--- app.js
+++ app.js
@@ -8,70 +8,70 @@ console.log("hello from app.js");
 
 // function findNotConsecutive(arr){
 //     let prev = arr[0];
-//     let temp; 
+//     let temp;
 //     for(let i =1; i<arr.length; i++) {
 //         if(prev + 1 != arr[i]) {
 //           temp=arr[i];
 //           return temp;
 //           break;
-//         } 
+//         }
 //         prev++;
 //     }
 //     return null;
 //     }
 
-    // console.log('Solution for findNotConsecutive:');
-    // console.log('numbers = [0,1, 2, 3, 4, 5, 6, 7, 8, 9];')
-    // console.log(findNotConsecutive(numbers));
-    // console.log('numbers2= [1,2,3,4,5,8];')
-    // console.log(findNotConsecutive(numbers2));
-    // console.log('Solution for findNotConsecutive([4,5,6,7,9,10]));')
-    // console.log(findNotConsecutive([4,5,6,7,9,10]));
-    // console.log('find from negative ');
-    // console.log(findNotConsecutive(negative));
+// console.log('Solution for findNotConsecutive:');
+// console.log('numbers = [0,1, 2, 3, 4, 5, 6, 7, 8, 9];')
+// console.log(findNotConsecutive(numbers));
+// console.log('numbers2= [1,2,3,4,5,8];')
+// console.log(findNotConsecutive(numbers2));
+// console.log('Solution for findNotConsecutive([4,5,6,7,9,10]));')
+// console.log(findNotConsecutive([4,5,6,7,9,10]));
+// console.log('find from negative ');
+// console.log(findNotConsecutive(negative));
 
-    // function multiply (arr) {
-    //   const multiplication = arr.reduce((accumulator, current) => {
-    //     return accumulator * current;
-    //   });
-    //   return multiplication;
-    // };
+// function multiply (arr) {
+//   const multiplication = arr.reduce((accumulator, current) => {
+//     return accumulator * current;
+//   });
+//   return multiplication;
+// };
 
-    // console.log('SOLUTION FOR MULTIPLICATION:')
-    // console.log(" multiply([1,2,3,4])")
-    // const multiplication = multiply([1,2,3,4]);
-    // console.log(multiply([1,2,3,4]));
+// console.log('SOLUTION FOR MULTIPLICATION:')
+// console.log(" multiply([1,2,3,4])")
+// const multiplication = multiply([1,2,3,4]);
+// console.log(multiply([1,2,3,4]));
 
-    // let hello = "hello";
-    // let HELLO = 'HELLO';
+// let hello = "hello";
+// let HELLO = 'HELLO';
 
-    // function isAllCaps (someString) {
-    //    if(someString == someString.toLocaleUpperCase()) {
-    //     return true;
-    //    } else {
-    //     return false;
-    //    };
-    // }
-    // console.log('Solution for isAllCaps:');
-    // console.log('isAllCaps(hello)')
-    // console.log(isAllCaps(hello));
-    // console.log('isAllCaps(HELLO)')
-    // console.log(isAllCaps(HELLO));
-    // console.log('isAllCaps(Ania)')
-    // console.log(isAllCaps("Ania"));
+// function isAllCaps (someString) {
+//    if(someString == someString.toLocaleUpperCase()) {
+//     return true;
+//    } else {
+//     return false;
+//    };
+// }
+// console.log('Solution for isAllCaps:');
+// console.log('isAllCaps(hello)')
+// console.log(isAllCaps(hello));
+// console.log('isAllCaps(HELLO)')
+// console.log(isAllCaps(HELLO));
+// console.log('isAllCaps(Ania)')
+// console.log(isAllCaps("Ania"));
 
-    // function reverse (word) {
-    //     const reversedWord = [];
-    //     for(let a=word.length; a >= 0; a--) {
-    //         reversedWord.push(word.charAt(a));
-    //         // console.log(reversedWord);
-    //     }
-    //     return reversedWord.join('');
-    // }
+// function reverse (word) {
+//     const reversedWord = [];
+//     for(let a=word.length; a >= 0; a--) {
+//         reversedWord.push(word.charAt(a));
+//         // console.log(reversedWord);
+//     }
+//     return reversedWord.join('');
+// }
 
-    // console.log('Solution for reverse(katar)');
-    // console.log(reverse('katar'));
-    // const aristotelis = 'Werner Vígi';
+// console.log('Solution for reverse(katar)');
+// console.log(reverse('katar'));
+// const aristotelis = 'Werner Vígi';
 //     Test.assertEquals(billboard("Jeong-Ho Aristotelis"), 600);
 // Test.assertEquals(billboard("Abishai Charalampos"), 570);
 // Test.assertEquals(billboard("Idwal Augustin"), 420);
@@ -83,7 +83,7 @@ console.log("hello from app.js");
 // Test.assertEquals(billboard("Paolo Oli"), 270);
 // Test.assertEquals(billboard("Hjalmar Liupold",40), 600);
 // Test.assertEquals(billboard("Simon Eadwulf"), 390);
-    
+
 //     function printNameOnBillboardCalculatePrice(name, price=15) {
 //         // const characters = name.length;
 //         // const billboard = characters * 30;
@@ -93,12 +93,12 @@ console.log("hello from app.js");
 //             total = total + price;
 //         }
 //         return total;
-     
+
 //     }
 //     console.log(printNameOnBillboardCalculatePrice(aristotelis));
 
 //     const square = [1,2,2];
-    
+
 //     function squareSum(numbers) {
 //         let sum = 0;
 //         for(let d = 0; d < numbers.length; d++) {
@@ -117,7 +117,7 @@ console.log("hello from app.js");
 //                  noDuplicates.push(a[e]);
 //                } else {
 //                  continue;
-//                }     
+//                }
 //         }
 //       return noDuplicates;
 //       }
@@ -168,7 +168,6 @@ console.log("hello from app.js");
 //   }
 //   console.log(countSheep(5));
 
-
 //   function removeChar(str){
 //     let last = str.length - 1;
 //     // debugger;
@@ -188,7 +187,6 @@ console.log("hello from app.js");
 //    console.log(bonusTime(4000, false));
 //    console.log(bonusTime(4000, true));
 
-
 //    function isIsogram(str) {
 //      if(str.length = 0) {
 //       return true;
@@ -197,17 +195,17 @@ console.log("hello from app.js");
 //     }
 
 //     let changeToArr =str.split("");
-//     let sortedArray = changeToArr.sort(); 
+//     let sortedArray = changeToArr.sort();
 //     console.log(sortedArray);
 
 //      for(let g=0; g < str.length; g++) {
 //       if (sortedArray[g] == sortedArray[g+1]) {
 //         return false;
 //       }
-//     } 
+//     }
 //       return true;
 //    }
-  
+
 //    console.log(isIsogram("Anna"));
 //    console.log(isIsogram("nikos"));
 //    console.log(isIsogram(""));
@@ -220,46 +218,68 @@ console.log("hello from app.js");
 //     nameArr = name.split(" ");
 //     initials = [];
 //     console.log(nameArr);
-    
-//     initials.push(nameArr[0].charAt(0).toUpperCase()); 
+
+//     initials.push(nameArr[0].charAt(0).toUpperCase());
 //     initials.push(nameArr[1].charAt(0));
 //     return initials.join(".");
 //    }
 //    console.log(abbrevName("anna Szpecht"));
 
-  //  const sequenceSum = (begin, end, step) => {
-  //   let sum = 0;
-  //   if(step > end) {
-  //     return 0;
-  //   } else {
-  //     for(let i = begin; i <= end; i=i+step) {
-  //       sum = i + sum;
-  //     }
-  //   }
-  //  return sum;
-  //  }
+//  const sequenceSum = (begin, end, step) => {
+//   let sum = 0;
+//   if(step > end) {
+//     return 0;
+//   } else {
+//     for(let i = begin; i <= end; i=i+step) {
+//       sum = i + sum;
+//     }
+//   }
+//  return sum;
+//  }
 
-  //  console.log(sequenceSum(2,6,2));
-  //  console.log(sequenceSum(1,5,3));
+//  console.log(sequenceSum(2,6,2));
+//  console.log(sequenceSum(1,5,3));
 
-  function arithmetic(a, b, operator){
-    let result = 0;
-    switch(operator){
-        case "add":
-          result = a + b;
-        break;
-        case "subract":
-          result= a - b;
-          break;
-        case "multiply":
-          result = a  * b;
-          break;
-        case "divide":
-          result = a / b;
-        break;
-        default:
-          return result;
-    }
-    return result;
+//   function arithmetic(a, b, operator){
+//     let result = 0;
+//     switch(operator){
+//         case "add":
+//           result = a + b;
+//         break;
+//         case "subract":
+//           result= a - b;
+//           break;
+//         case "multiply":
+//           result = a  * b;
+//           break;
+//         case "divide":
+//           result = a / b;
+//         break;
+//         default:
+//           return result;
+//     }
+//     return result;
+//   }
+// console.log(arithmetic(2, 2, "add"));
+
+function getCount(str) {
+  let vowCount = 0;
+  vow = "";
+  for (let i = 0; i <= str.length; i++) {
+    
+    if(str.charAt(i) == "a"){
+      vowCount = vowCount + 1;
+    } else if(str.charAt(i) == "e") {
+      vowCount++;
+    } else if(str.charAt(i) == "i") {
+      vowCount++;
+    } else if(str.charAt(i) == "o") {
+      vowCount++;
+    } else if(str.charAt(i) == "u") {
+      vowCount++;
+    } 
   }
-console.log(arithmetic(2, 2, "add"));
+  return vowCount;
+}
+
+console.log(getCount("abracadabra"));
